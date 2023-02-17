de592ef419367539854715f7e5bc415ee8c5e4e8
Daniel Yakovlev <Daniel.yakovlev@gmail.com>
2023-02-17 10:41:24 -0800

    added item control

--- components/Header.js
+++ components/Header.js
@@ -2,14 +2,21 @@ import { Text, View, StyleSheet } from "react-native";
 
 const Header = () => {
   return (  
-    <View style={styles.header}>
+    <View style={styles.headerContainer}>
       <Text style={styles.header}>Clothing App</Text>
     </View>
    );
 }
 
 const styles = StyleSheet.create({
+  headerContainer: {
+    paddingTop: 40,
+    paddingBottom: 10,
+    justifyContent: "center",
+    backgroundColor: '#fab9d8'
+  },
   header: {
+    textAlign: "center",
     fontSize: 30
   }
 })
