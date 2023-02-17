6c87b7a9d885a208c98ac05bec645509954a45dc
Daniel Yakovlev <yakovdan@uw.edu>
2023-02-17 14:24:53 -0800

    used controller

--- components/ItemForm.js
+++ components/ItemForm.js
@@ -1,25 +1,36 @@
 import React from 'react';
 import { v4 } from 'uuid';
-import { useForm } from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
+import { Button, TextInput, View } from 'react-native';
 
 const ItemForm = (props) => {
-  const { register, handleSubmit} = useForm(); 
+  const {
+    control,
+    handleSubmit,
+    formState: {errors, isValid}
+  } = useForm({mode: 'onBlur'})
 
   const onSubmit = data => console.log(data);
-  // function onSubmit(event) {
-  //   event.preventDefault();
-  //   console.log(event);
-  //   props.onFormSubmit({
-  //   });
-  // }
+
 
   return ( 
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register("name")}/>
-      <input {...register("desc")}/>
-      <input type="submit"/>
-      
-    </form>
+    <View>
+      <Controller 
+        control={control}
+        name="name"
+        render={({field: {onChange, value, onBlur}}) => (
+          <TextInputField
+            iconName = "name"
+            iconType = "MaterialIcons"
+            placeholder = "name"
+            value ={value}
+            onBlur = {onBlur}
+            onChangeText = {value => onChange(value)}
+          />
+        )}
+      />
+      <Button title='Submit' onPress={handleSubmit(onSubmit)} />
+    </View>
    );
 }
  
