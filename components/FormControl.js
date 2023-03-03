77f1f3476f676acff75fd33730706b911398639b
Daniel Yakovlev <daniel.yakovlev@gmail.com>
2023-03-03 14:39:30 -0800

    updated formcontrol

--- components/FormControl.js
+++ components/FormControl.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { View, Button } from 'react-native';
+import { View, Button, StyleSheet } from 'react-native';
 import AddClothes from './AddClothes';
 import AddOutfits from './AddOutfits';
+import FormButton from './FormButton';
 
 const FormControl = () => {
   const [showForm, setForm] = useState(0); //0 = default, 1 = clothes, 2 = outfits
@@ -10,8 +11,9 @@ const FormControl = () => {
     <View>
       {showForm === 0 &&
         <>
-          <Button title="Add Clothes" onPress={() => setForm(1)} />
-          <Button title="Add Outfits" onPress={() => setForm(2)} />
+            <Button title="Add Clothes" style={styles.FormButton} onPress={() => setForm(1)} />
+            <Button title="Add Outfits" style={styles.FormButton} onPress={() => setForm(2)} />
+
         </>
       }
       {showForm === 1 &&
@@ -21,10 +23,14 @@ const FormControl = () => {
         <AddOutfits />
       }
       {showForm !== 0 &&
-        <Button title="Back" onPress={() => setForm(0)} />
+        <View style={styles.FormButton}>
+          <Button title="Back" onPress={() => setForm(0)} />
+        </View>
       }
     </View>
   );
 }
+
+
 
 export default FormControl;
