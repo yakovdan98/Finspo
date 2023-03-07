54be0620206ec58ad815ed010fa469e732f1490b
Daniel Yakovlev <Daniel.yakovlev@gmail.com>
2023-03-07 11:14:47 -0800

    changed additems.js to use inputfield

--- components/AddItems.js
+++ components/AddItems.js
@@ -6,13 +6,11 @@ import * as ImagePicker from 'expo-image-picker';
 import { MediaTypeOptions } from 'expo-image-picker';
 
 import FormButton from './FormButton';
+import InputField from './InputField';
 
 
 const AddItems = (props) => {
   const { control, handleSubmit, formState: { errors } } = useForm({
-    defaultValues: {
-      name: ''
-    }
   });
 
   const [image, setImage] = useState(null);
@@ -40,74 +38,43 @@ const AddItems = (props) => {
 
 
   return (
+    <View>
+
       <View>
+        <Text style={styles.FormHeader}>Adding {formType}</Text>
+      </View>
+      {image
+        ?
+        <>
+          <FormButton onPress={addImage} title="Change Image" />
+          <Image source={{ uri: image }} style={{ width: 400, height: 400 }} />
+        </>
+        : <FormButton onPress={addImage} title="Add Image" />
+      }
 
-        <View>
-          <Text style={styles.FormHeader}>Adding {formType}</Text>
-        </View>
-        {image
-          ?
-          <>
-            <FormButton onPress={addImage} title="Change Image" />
-            <Image source={{ uri: image }} style={{ width: 400, height: 400 }} />
-          </>
-          : <FormButton onPress={addImage} title="Add Image" />
-        }
-        <Controller
-          defaultValue=""
-          control={control}
-          rules={{
-            required: { value: true, message: 'name is required' }
-          }}
-          render={({ field: { onChange, value } }) => (
-            <TextInput
-              style={styles.input}
-              onChangeText={onChange}
-              value={value}
-              placeholder="Item"
-            />
-          )}
-          name="name"
-        />
-        {errors.name && <Text style={styles.errorMessage}>This is required.</Text>}
-
-        <Controller
-          defaultValue=""
-          control={control}
-          rules={{
-            required: { value: true, message: 'description is required' }
-          }}
-          render={({ field: { onChange, value } }) => (
-            <TextInput
-              style={styles.input}
-              onChangeText={onChange}
-              value={value}
-              placeholder="Description"
-            />
-          )}
-          name="desc"
-        />
-        {errors.desc && <Text style={styles.errorMessage}>This is required.</Text>}
+      <InputField
+        control={control}
+        name="Item"
+        errors={errors.Item}
+      />
+      <InputField 
+        control = {control}
+        name = "Description"
+        errors={errors.Description}
+      />
 
         <View style={styles.FormButton}>
-          <FormButton title="Submit" onPress={handleSubmit(onSubmit)} />
-
-        </View>
+        <FormButton title="Submit" onPress={handleSubmit(onSubmit)} />
 
       </View>
+
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     height: 600
-  },
-  input: {
-    borderWidth: 2,
-    borderColor: "#000",
-    margin: 5,
-    borderRadius: 10,
-    padding: 10
   },
   FormButton: {
     margin: 10
@@ -117,9 +84,6 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 'bold',
     textAlign: 'center',
-  },
-  errorMessage:{
-    color: 'red'
   }
 
 })
