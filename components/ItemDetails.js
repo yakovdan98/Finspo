1e0a301f216e26271a5e40b47a2b96233a90e307
Daniel Yakovlev <Daniel.yakovlev@gmail.com>
2023-03-08 15:04:13 -0800

    created detail view page

---
+++ components/ItemDetails.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Text, View, StyleSheet, Image } from 'react-native';
+const ItemDetails = ({ item }) => {
+  return (
+    <View style={styles.container}>
+      
+      <View style={styles.item}>
+        <Image source={{ uri: item.image }} resizeMode={'cover'} style={{ width: '100%', height: '100%' }} />
+      </View>
+      <Text>{item.description}</Text>
+    </ View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor : 'white',
+    paddingBottom: 20,
+    borderRadius: 10,
+
+  },
+  item: {
+    width: '90%',
+    aspectRatio: 1,
+    backgroundColor: 'white',
+    margin: '1%',
+    borderRadius: 10,
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.26,
+    shadowRadius: 6,
+    elevation: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    overflow: 'hidden',
+  },
+});
+export default ItemDetails;
