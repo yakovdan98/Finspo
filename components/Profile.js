60c620f048dfb065cadbc34a7131254f7c3630da
Daniel Yakovlev <Daniel.yakovlev@gmail.com>
2023-03-08 16:46:48 -0800

    updated profile.js

--- components/Profile.js
+++ components/Profile.js
@@ -5,19 +5,38 @@ import Header from './Header';
 import OutfitList from './OutfitList';
 import { useClothes } from '../contexts/clothes';
 import { useOutfits } from '../contexts/outfits';
+import { SelectedContext } from '../contexts/SelectedContext';
+import ItemDetails from './ItemDetails';
+import FormButton from './FormButton';
+import { View } from 'react-native';
 
 const Profile = () => {
   const { clothes } = useClothes();
   const { outfits } = useOutfits();
-  return (
-    <>
-      <Header title='Profile' />
-      <ScrollView>
-        <ItemList type='Clothes' list={clothes}/>
-        <ItemList type='Outfits' list={outfits}/>
-      </ScrollView>
-    </>
-  );
+  const [selectedItem, setSelectedItem] = React.useState(null);
+
+  if (selectedItem) {
+    return (
+      <>
+        <Header title={selectedItem.name} />
+        <FormButton title="Back" onPress={() => setSelectedItem(null)} />
+        <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+          <ItemDetails item={selectedItem} />
+        </View >
+      </>
+    );
+  } else {
+    return (
+      <SelectedContext.Provider value={{ selectedItem, setSelectedItem }}>
+        <Header title='Profile' />
+        <ScrollView>
+          <ItemList type='Clothes' list={clothes} />
+          <ItemList type='Outfits' list={outfits} />
+        </ScrollView>
+      </SelectedContext.Provider>
+    );
+
+  }
 }
 
 export default Profile;
