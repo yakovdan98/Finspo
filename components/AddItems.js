be83235d8c0a864bf6c75317e90f94feed08d260
Daniel Yakovlev <Daniel.yakovlev@gmail.com>
2023-03-09 09:27:51 -0800

    added drop down menu for type of item

--- components/AddItems.js
+++ components/AddItems.js
@@ -1,9 +1,10 @@
 import React, { useContext, useState } from 'react';
 import { v4 } from 'uuid';
 import { useForm, Controller } from 'react-hook-form';
-import { Text, Image, TextInput, View, StyleSheet, ScrollView } from 'react-native';
+import { Image, TextInput, View, StyleSheet, ScrollView } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import { MediaTypeOptions } from 'expo-image-picker';
+import DropDownPicker from 'react-native-dropdown-picker';
 
 import FormButton from './FormButton';
 import InputField from './InputField';
@@ -16,12 +17,21 @@ const AddItems = (props) => {
   const { control, handleSubmit, formState: { errors } } = useForm({});
 
   const [image, setImage] = useState(null);
-  const {clothes, clothesUpdate} = useClothes();
-  const {outfits, outfitUpdate} = useOutfits();
+  const { clothes, clothesUpdate } = useClothes();
+  const { outfits, outfitUpdate } = useOutfits();
   const formType = (props.type === 1) ? "Clothes" : "Outfits";
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(null);
+  const [items, setItems] = useState([
+    { label: 'Top', value: 'top' },
+    { label: 'Bottom', value: 'bottom' },
+    { label: 'Shoes', value: 'shoes' },
+    { label: 'Accessory', value: 'accessory' },
+  ]);
 
 
 
+  //todo: seperate into hook
   const addImage = async () => {
     let _image = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
@@ -37,7 +47,7 @@ const AddItems = (props) => {
   }
 
   const onSubmit = (data) => {
-    
+
     if (props.type === 1) {
       const newClothes = {
         id: v4(),
@@ -45,7 +55,7 @@ const AddItems = (props) => {
         description: data.Description,
         image: image,
       }
-      
+
       clothesUpdate(clothes.concat(newClothes));
       console.log(clothes);
     }
@@ -79,15 +89,28 @@ const AddItems = (props) => {
         name="Item"
         errors={errors.Item}
       />
-      <InputField 
-        control = {control}
-        name = "Description"
+      <InputField
+        control={control}
+        name="Description"
         errors={errors.Description}
       />
 
-        <View style={styles.FormButton}>
+      {props.type === 1 &&
+        <DropDownPicker
+          placeholder="Select Type of Item"
+          listMode='MODAL'
+          open={open}
+          value={value}
+          items={items}
+          setOpen={setOpen}
+          setValue={setValue}
+          setItems={setItems}
+          modalAnimationType="slide"
+          style={styles.DropDown}
+        />
+      }
+      <View style={styles.FormButton}>
         <FormButton title="Submit" onPress={handleSubmit(onSubmit)} />
-
       </View>
 
     </View>
@@ -106,7 +129,16 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 'bold',
     textAlign: 'center',
-  }
+  },
+  DropDown: {
+    backgroundColor: '#fafafa',
+    borderWidth: 2,
+    borderColor: "#000",
+    margin: 5,
+    width: 'auto',
+    borderRadius: 10,
+    padding: 10
+  },
 
 })
 
